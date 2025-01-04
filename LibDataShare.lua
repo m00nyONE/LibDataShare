ba0cc95fec47c61d47d889bc0d760e6dc3b551a4
m00nyONE <severin.meier.x@gmail.com>
2025-01-04 16:20:18 +0100

    move PING_RATE modifier to init function

--- LibDataShare.lua
+++ LibDataShare.lua
@@ -12,11 +12,6 @@ local ENABLED = true -- this lib won't send or process map pings if this setting
 local MAIN_MAP_INDEX = 30 -- Vvardenfell
 local PING_RATE = 2020 -- minimum time between pings
 
-if GetAPIVersion() >= 101045 then
-	-- set the ping rate to every 10seconds because of API changes in U45
-	PING_RATE = 10020
-end
-
 local lastPingTime = 0 -- time of the latest map ping
 local lastOnPingTime = 0 -- time of the latest received and processed map ping
 
@@ -333,6 +328,11 @@ local function OnMapPing(eventCode, pingEventType, pingType, pingTag, offsetX, o
 end
 
 local function Initialize()
+	-- set the ping rate to every 10seconds because of API changes in U45
+	if GetAPIVersion() >= 101045 then
+		PING_RATE = 10020
+	end
+
 	local function OnPlayerActivated()
 		-- Unregister map ping handler.
 		EM:UnregisterForEvent(NAME, EVENT_MAP_PING)
